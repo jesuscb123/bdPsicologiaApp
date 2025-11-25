528f83dadc308379ff5339d5e2431d64e940f936
jesuscb123 <cbjesus@iesrafaelalberti.es>
2025-11-25 14:40:37 +0100

    Modificdo usuario para que devuelva el firebaseUid

--- src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/dto/usuarioDTO/UsuarioResponse.kt
+++ src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/dto/usuarioDTO/UsuarioResponse.kt
@@ -2,6 +2,7 @@ package dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.web.dto.usuarioDTO
 
 data class UsuarioResponse(
     val id: Long?,
+    val firebaseUid: String,
     val email: String,
     val nombreUsuario: String
 )
