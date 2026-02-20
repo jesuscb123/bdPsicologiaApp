a6819a6016143fb79ef38a52728ed64f9f382312
jesuscb123 <cbjesus@iesrafaelalberti.es>
2026-02-20 13:56:33 +0100

    modificando paciente repository

--- src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/repository/PacienteRepository.kt
+++ src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/repository/PacienteRepository.kt
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface PacienteRepository : JpaRepository<Paciente, Long> {
-    @Query("SELECT p FROM Paciente p WHERE p.usuario.firebaseUid = :fireBaseId")
+    @Query("SELECT p FROM Paciente p WHERE p.usuario.firebaseUid = :firebaseUidUsuario")
     fun findByIdFirebaseUsuario(firebaseUidUsuario: String): Paciente?
 
     fun existsByUsuario(usuario: Usuario): Boolean
