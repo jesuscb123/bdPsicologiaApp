66c42b06b3ea820ce72cd1e8ba04f9da50d2f309
jesuscb123 <cbjesus@iesrafaelalberti.es>
2026-02-23 11:47:34 +0100

    Refactorizando para que no sea tan pesada las llamadas endpoint

--- src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/mapper/UsuarioMapper.kt
+++ src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/mapper/UsuarioMapper.kt
@@ -16,7 +16,6 @@ object UsuarioMapper {
     fun toResponse(usuario: Usuario): UsuarioResponse {
         return UsuarioResponse(
             usuario.id!!,
-            usuario.firebaseUid,
             usuario.email,
             usuario.nombreUsuario
         )
