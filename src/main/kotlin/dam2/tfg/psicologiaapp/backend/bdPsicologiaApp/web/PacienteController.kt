1ac1d0bffdd99b91962834640a1f981c0589c3f9
jesuscb123 <cbjesus@iesrafaelalberti.es>
2026-03-11 12:26:54 +0100

    eliminado dos endpoints de pacientes y psicologos innecesarios

--- src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/PacienteController.kt
+++ src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/PacienteController.kt
@@ -1,23 +1,14 @@
 package dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.web
 
-import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.domain.FirebaseUserData
-import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.service.FirebaseService
 import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.service.IServicioPaciente
-import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.service.IServicioUsuario
-import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.web.dto.usuarioDTO.PacienteRequest
 import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.web.dto.usuarioDTO.PacienteResponse
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
-import java.net.URI
-import org.springframework.http.HttpStatus
 
 @RestController
 @RequestMapping("/api/pacientes")
 class PacienteController(
-    private val  servicioPaciente: IServicioPaciente,
-    private val firebaseService: FirebaseService,
-    private val servicioUsuario: IServicioUsuario
+    private val  servicioPaciente: IServicioPaciente
 ) : IController{
     @GetMapping
     fun obtenerPacientes(): List<PacienteResponse>{
@@ -44,32 +35,6 @@ class PacienteController(
             ResponseEntity.ok(paciente)
         }else{
             ResponseEntity.notFound().build()
-        }
-    }
-
-    @PostMapping
-    fun crearPaciente(
-        @AuthenticationPrincipal usuarioFirebase: FirebaseUserData,
-        @RequestBody pacienteRequest: PacienteRequest
-    ): ResponseEntity<Any> {
-
-        return try {
-            val pacienteResponse = servicioUsuario.crearUsuario(
-                fireBaseUid = usuarioFirebase.uid,
-                email = usuarioFirebase.email,
-                request = pacienteRequest
-            )
-
-            // 2. Retornamos el DTO de respuesta con un 201 Created
-            ResponseEntity.created(URI.create("/api/pacientes/${pacienteResponse.id}"))
-                .body(pacienteResponse)
-
-        } catch (e: IllegalStateException) {
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
-
-        } catch (e: Exception) {
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Error técnico: ${e.message} | Causa: ${e.cause}")
         }
     }
 }
--- src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/PsicologoController.kt
+++ src/main/kotlin/dam2/tfg/psicologiaapp/backend/bdPsicologiaApp/web/PsicologoController.kt
@@ -1,9 +1,6 @@
 package dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.web
 
-import com.google.firebase.auth.FirebaseAuthException
 import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.domain.FirebaseUserData
-import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.domain.Rol
-import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.service.FirebaseService
 import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.service.IServicioPsicologo
 import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.service.IServicioUsuario
 import dam2.tfg.psicologiaapp.backend.bdPsicologiaApp.web.dto.usuarioDTO.PsicologoRequest
@@ -48,31 +45,6 @@ class PsicologoController(
             ResponseEntity.ok(psicologo)
         }else{
             ResponseEntity.notFound().build()
-        }
-    }
-
-    @PostMapping
-    fun crearPsicologo(
-        @AuthenticationPrincipal usuarioFirebase: FirebaseUserData,
-        @RequestBody psicologoRequest: PsicologoRequest
-    ): ResponseEntity<Any> {
-
-        return try {
-            val psicologoResponse = servicioUsuario.crearUsuario(
-                fireBaseUid = usuarioFirebase.uid,
-                email = usuarioFirebase.email,
-                request = psicologoRequest
-            )
-
-            ResponseEntity.created(URI.create("/api/psicologos/${psicologoResponse.id}"))
-                .body(psicologoResponse)
-
-        } catch (e: IllegalStateException) {
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
-
-        } catch (e: Exception) {
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Error interno del servidor: ${e.message}")
         }
     }
 }
